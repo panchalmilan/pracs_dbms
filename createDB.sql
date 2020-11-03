f3e6a56b7ea9da460e10e0bd1df62cadcb8e30df
panchalmilan <mpanchal1310@gmail.com>
2020-11-03 21:31:20 +0530

    post links table created

--- createDB.sql
+++ createDB.sql
@@ -6,3 +6,7 @@ USE `social_network`;
 CREATE TABLE `post_images`(
   `post_image` VARCHAR(255) NOT NULL PRIMARY KEY
 );
+
+CREATE TABLE `post_links`(
+  `post_link` VARCHAR(255) NOT NULL PRIMARY KEY
+);
