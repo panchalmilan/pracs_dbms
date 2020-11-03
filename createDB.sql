13b6b69353e15c12603d42b754cc2304e3a32cb9
panchalmilan <mpanchal1310@gmail.com>
2020-11-03 22:07:20 +0530

    added posts

--- createDB.sql
+++ createDB.sql
@@ -3,6 +3,7 @@ CREATE DATABASE `social_network`;
 USE `social_network`;
 
 
+
 CREATE TABLE `post_images`(
   `post_image` VARCHAR(255) NOT NULL PRIMARY KEY
 );
@@ -30,4 +31,20 @@ CREATE TABLE `group`(
 
 ALTER TABLE `group_member`
   ADD FOREIGN KEY (`groupId`)
-      REFERENCES `group` (`groupId`);
+    REFERENCES `group` (`groupId`);
+
+
+CREATE TABLE `posts`(
+  `postId` INT NOT NULL PRIMARY KEY,
+  `num_likes` INT NULL DEFAULT 0,
+  `num_dislikes` INT NULL DEFAULT 0,
+  `post_text` VARCHAR(255) NULL,
+  `create_time` DATE NOT NULL,
+  `post_link` VARCHAR(255) NOT NULL,
+  `post_image`VARCHAR(255) NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+  FOREIGN KEY (`post_link`)
+    REFERENCES `post_links` (`post_link`),
+  FOREIGN KEY (`post_image`)
+    REFERENCES `post_images` (`post_image`)
+);
